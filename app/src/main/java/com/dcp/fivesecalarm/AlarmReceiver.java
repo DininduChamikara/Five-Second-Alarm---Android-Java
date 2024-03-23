70e8a7cd93a104c14e250470aab7a95ef28f8e0f
DininduChamikara <dininduchamikara99@gmail.com>
2024-03-23 22:10:53 +0530

    fixed issues

--- app/src/main/java/com/dcp/fivesecalarm/AlarmReceiver.java
+++ app/src/main/java/com/dcp/fivesecalarm/AlarmReceiver.java
@@ -3,6 +3,9 @@ package com.dcp.fivesecalarm;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.widget.Toast;
 
 public class AlarmReceiver extends BroadcastReceiver {
